6243c943b0f28f8fc8f3be600eaa360a58e7d8b7
Max han <mwellh84@gmail.com>
2017-11-15 02:28:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ function Scooter(year, color,model ){
 
 function Driver(name, age, experience) {
   this.name = name
-  this.age = age 
+  this.age = age
   this.experience = experience
+}
+
+function PickupLocation(address, city){
+  this.address = address 
+  this.city = city 
 }
